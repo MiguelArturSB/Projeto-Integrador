986131bf058f42d7543e072c21a71e10899c5be5
MiguelAr <miguelarthurdasilvabarros@gmail.com>
2025-05-23 11:19:23 -0300

    feito proferssor

--- front-end/src/app/professor/page.jsx
+++ front-end/src/app/professor/page.jsx
@@ -1,105 +1,286 @@
 "use client"
 
-import { useState } from 'react';
-import './prof.css';
+import { useState } from "react";
 
+export default function ProfessorTable() {
+  const [presence, setPresence] = useState(true);
 
-
-export default function professor() {
-    const [clicado, setClicado] = useState(false);
-      const toggleClicado = () => {
-    setClicado(!clicado);
+  const togglePresence = () => {
+    setPresence((prev) => !prev);
   };
-      
-    return (
-        <>
-        
-            <div className='bg-[#c9e8ff] w-[100%] h-screen items-center mx-auto flex justify-center '>
-                <div className='mt-[60px] rendondo_grand w-[1450px] h-[850px] mx-auto flex items-center justify-center bg-[#899197]'>
-               
-                <nav className='
-                rounded-full 
-                absolute 
-                w-[150px] h-[50px] 
-                bg-[#d8dde0]  
-                z-10 
-                items-center text-center flex justify-center
-                top-45 left-140
-                 shadow_nim'>
-                    <p className='font-bold text-xl'>Turma:2md</p>
-                    </nav>
 
-                    <nav className='
-                rounded-full 
-                absolute 
-                w-[150px] h-[50px] 
-                bg-[#d8dde0]  
-                z-10 
-                items-center text-center flex justify-center
-                top-45 left-210
-                shadow_nim'>
-                    <p className='font-bold text-xl'>Materia:LER</p>
-                    </nav>
-                
-                <div className='
-        w-[1400px] 
-        h-[800px] 
-        overflow-y-auto
-        bg-[#b2bac0] 
-        rendondo_pequeno'>
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-slate-100 to-slate-300 flex items-center justify-center p-8
+    
+    ">
+      <div className="bg-white shadow-2xl rounded-3xl w-full max-w-6xl p-6 relative overflow-hidden">
+        <h1 className=" font-bold text-gray-800 mb-6 border-b pb-4 text-xl 
+        sm:text-4xl
+        ">Painel da Presença</h1>
 
-        <div className='m-auto  w-[1350px] h-[60px] bg-[#d8dde0] rounded-full shadow_nim mt-[45] pl-[130px] pr-[270px] justify-between flex items-center'>
-
-        <p className=' text-2xl'>Nome</p>
-        <p className='f text-2xl'>RA</p>
-        <p className=' text-2xl'>Frequencia</p>
+        <div className="flex  flex-row justify-between mb-6 gap-4
+        sm:flex-row
+        ">
+          <div className="bg-sky-100 text-sky-800 px-3 py-3 rounded-full font-medium shadow-inner 
+          text-sm sm:text-xl sm:px-6
+          ">
+            Turma: <span className="font-semibold">2MD</span>
+          </div>
+          <div className="bg-emerald-100 text-emerald-800 px-3 py-3 rounded-full font-medium shadow-inner
+           text-sm sm:text-xl  sm:px-6">
+            Matéria: <span className="font-semibold">LER</span>
+          </div>
         </div>
 
+        <div className="overflow-x-auto">
+          <table className="min-w-full table-fixed rounded-xl overflow-hidden shadow-md
+          sm:table-auto
+          
+          ">
+            <thead>
+              <tr className="bg-gradient-to-r from-slate-700 to-slate-900 text-white text-left text-sm 
+              sm:text-lg
+              
+              ">
+                <th className=" px-6   font-semibold text-xs sm:text-sm">Nome</th>
+                <th className="py-4 px-6 font-semibold  text-xs sm:text-sm">RA</th>
+                <th className="py-4 px-6 font-semibold  text-xs sm:text-sm">Frequência</th>
+                <th className="py-4 px-6 font-semibold  text-xs sm:text-sm">Presente</th>
+              </tr>
+            </thead>
+            <tbody>
 
-<div className='m-auto w-[1350px] h-[60px] bg-[#d8dde0] rounded-full shadow_nim mt-[25] pl-[50px] pr-[10px]  justify-between flex items-center'>
+              <tr className="odd:bg-white even:bg-slate-100 hover:bg-slate-200 transition-all">
+                <td className="py-1 px-6 text-gray-700  text-xs sm:text-sm sm:py-4">Miguel Artur da Silva Barros</td>
+                <td className="py-4 px-6 text-gray-700  text-xs sm:text-sm">10000000001</td>
+                <td className="py-4 px-6 text-gray-700  text-xs sm:text-sm">100%</td>
+                <td className="py-4 px-6">
+                  <button
+                    onClick={togglePresence}
+                    className={`w-6 h-6 rounded-full border-4 transition-colors duration-300 ${
+                      presence ? "bg-red-500 border-red-700" : " bg-green-500 border-green-700"
+                    }`}
+                    title={presence ? "Ausente" : "Presente"}
+                  ></button>
+                </td>
+              </tr>
 
-        <p className=' text-2xl'>Miguel Artur da Silva Barros</p>
-        <p className='f text-2xl pr-[200px]'>10000000001</p>
-        <p className=' text-2xl pr-[60px]'>100%</p>
-        
-         <button
-      onClick={toggleClicado}
-      className={`cursor-pointer rounded-full w-[70px] h-[50px] transition-colors duration-300 ${
-        clicado ? 'bg-[#00bf63]' : 'bg-[#899197]'
-      }`}
-    ></button>
 
+              <tr className="odd:bg-white even:bg-slate-100 hover:bg-slate-200 transition-all">
+                <td className="py-1 px-6 text-gray-700  text-xs sm:text-sm sm:py-4">Miguel Artur da Silva Barros</td>
+                <td className="py-4 px-6 text-gray-700  text-xs sm:text-sm">10000000001</td>
+                <td className="py-4 px-6 text-gray-700  text-xs sm:text-sm">100%</td>
+                <td className="py-4 px-6">
+                  <button
+                    onClick={togglePresence}
+                    className={`w-6 h-6 rounded-full border-4 transition-colors duration-300 ${
+                      presence ? "bg-red-500 border-red-700" : " bg-green-500 border-green-700"
+                    }`}
+                    title={presence ? "Ausente" : "Presente"}
+                  ></button>
+                </td>
+              </tr>
+
+
+
+
+
+              <tr className="odd:bg-white even:bg-slate-100 hover:bg-slate-200 transition-all">
+                <td className="py-1 px-6 text-gray-700  text-xs sm:text-sm sm:py-4">Miguel Artur da Silva Barros</td>
+                <td className="py-4 px-6 text-gray-700  text-xs sm:text-sm">10000000001</td>
+                <td className="py-4 px-6 text-gray-700  text-xs sm:text-sm">100%</td>
+                <td className="py-4 px-6">
+                  <button
+                    onClick={togglePresence}
+                    className={`w-6 h-6 rounded-full border-4 transition-colors duration-300 ${
+                      presence ? "bg-red-500 border-red-700" : " bg-green-500 border-green-700"
+                    }`}
+                    title={presence ? "Ausente" : "Presente"}
+                  ></button>
+                </td>
+              </tr>
+
+
+
+
+
+
+
+
+
+
+              <tr className="odd:bg-white even:bg-slate-100 hover:bg-slate-200 transition-all">
+                <td className="py-1 px-6 text-gray-700  text-xs sm:text-sm sm:py-4">Miguel Artur da Silva Barros</td>
+                <td className="py-4 px-6 text-gray-700  text-xs sm:text-sm">10000000001</td>
+                <td className="py-4 px-6 text-gray-700  text-xs sm:text-sm">100%</td>
+                <td className="py-4 px-6">
+                  <button
+                    onClick={togglePresence}
+                    className={`w-6 h-6 rounded-full border-4 transition-colors duration-300 ${
+                      presence ? "bg-red-500 border-red-700" : " bg-green-500 border-green-700"
+                    }`}
+                    title={presence ? "Ausente" : "Presente"}
+                  ></button>
+                </td>
+              </tr>
+
+
+
+
+
+
+
+
+
+
+              <tr className="odd:bg-white even:bg-slate-100 hover:bg-slate-200 transition-all">
+                <td className="py-1 px-6 text-gray-700  text-xs sm:text-sm sm:py-4">Miguel Artur da Silva Barros</td>
+                <td className="py-4 px-6 text-gray-700  text-xs sm:text-sm">10000000001</td>
+                <td className="py-4 px-6 text-gray-700  text-xs sm:text-sm">100%</td>
+                <td className="py-4 px-6">
+                  <button
+                    onClick={togglePresence}
+                    className={`w-6 h-6 rounded-full border-4 transition-colors duration-300 ${
+                      presence ? "bg-red-500 border-red-700" : " bg-green-500 border-green-700"
+                    }`}
+                    title={presence ? "Ausente" : "Presente"}
+                  ></button>
+                </td>
+              </tr>
+
+
+
+
+
+
+
+
+
+
+              <tr className="odd:bg-white even:bg-slate-100 hover:bg-slate-200 transition-all">
+                <td className="py-1 px-6 text-gray-700  text-xs sm:text-sm sm:py-4">Miguel Artur da Silva Barros</td>
+                <td className="py-4 px-6 text-gray-700  text-xs sm:text-sm">10000000001</td>
+                <td className="py-4 px-6 text-gray-700  text-xs sm:text-sm">100%</td>
+                <td className="py-4 px-6">
+                  <button
+                    onClick={togglePresence}
+                    className={`w-6 h-6 rounded-full border-4 transition-colors duration-300 ${
+                      presence ? "bg-red-500 border-red-700" : " bg-green-500 border-green-700"
+                    }`}
+                    title={presence ? "Ausente" : "Presente"}
+                  ></button>
+                </td>
+              </tr>
+
+
+
+
+
+
+
+
+
+
+              <tr className="odd:bg-white even:bg-slate-100 hover:bg-slate-200 transition-all">
+                <td className="py-1 px-6 text-gray-700  text-xs sm:text-sm sm:py-4">Miguel Artur da Silva Barros</td>
+                <td className="py-4 px-6 text-gray-700  text-xs sm:text-sm">10000000001</td>
+                <td className="py-4 px-6 text-gray-700  text-xs sm:text-sm">100%</td>
+                <td className="py-4 px-6">
+                  <button
+                    onClick={togglePresence}
+                    className={`w-6 h-6 rounded-full border-4 transition-colors duration-300 ${
+                      presence ? "bg-red-500 border-red-700" : " bg-green-500 border-green-700"
+                    }`}
+                    title={presence ? "Ausente" : "Presente"}
+                  ></button>
+                </td>
+              </tr>
+
+
+
+
+
+
+
+
+
+              <tr className="odd:bg-white even:bg-slate-100 hover:bg-slate-200 transition-all">
+                <td className="py-1 px-6 text-gray-700  text-xs sm:text-sm sm:py-4">Miguel Artur da Silva Barros</td>
+                <td className="py-4 px-6 text-gray-700  text-xs sm:text-sm">10000000001</td>
+                <td className="py-4 px-6 text-gray-700  text-xs sm:text-sm">100%</td>
+                <td className="py-4 px-6">
+                  <button
+                    onClick={togglePresence}
+                    className={`w-6 h-6 rounded-full border-4 transition-colors duration-300 ${
+                      presence ? "bg-red-500 border-red-700" : " bg-green-500 border-green-700"
+                    }`}
+                    title={presence ? "Ausente" : "Presente"}
+                  ></button>
+                </td>
+              </tr>
+
+
+
+
+
+
+
+
+              <tr className="odd:bg-white even:bg-slate-100 hover:bg-slate-200 transition-all">
+                <td className="py-1 px-6 text-gray-700  text-xs sm:text-sm sm:py-4">Miguel Artur da Silva Barros</td>
+                <td className="py-4 px-6 text-gray-700  text-xs sm:text-sm">10000000001</td>
+                <td className="py-4 px-6 text-gray-700  text-xs sm:text-sm">100%</td>
+                <td className="py-4 px-6">
+                  <button
+                    onClick={togglePresence}
+                    className={`w-6 h-6 rounded-full border-4 transition-colors duration-300 ${
+                      presence ? "bg-red-500 border-red-700" : " bg-green-500 border-green-700"
+                    }`}
+                    title={presence ? "Ausente" : "Presente"}
+                  ></button>
+                </td>
+              </tr>
+
+
+
+
+
+
+
+
+
+              <tr className="odd:bg-white even:bg-slate-100 hover:bg-slate-200 transition-all">
+                <td className="py-1 px-6 text-gray-700  text-xs sm:text-sm sm:py-4">Miguel Artur da Silva Barros</td>
+                <td className="py-4 px-6 text-gray-700  text-xs sm:text-sm">10000000001</td>
+                <td className="py-4 px-6 text-gray-700  text-xs sm:text-sm">100%</td>
+                <td className="py-4 px-6">
+                  <button
+                    onClick={togglePresence}
+                    className={`w-6 h-6 rounded-full border-4 transition-colors duration-300 ${
+                      presence ? "bg-red-500 border-red-700" : " bg-green-500 border-green-700"
+                    }`}
+                    title={presence ? "Ausente" : "Presente"}
+                  ></button>
+                </td>
+              </tr>
+
+
+
+
+             
+            </tbody>
+
+
+          </table>
         </div>
 
+        <div className=" mt-[2%]  flex justify-end ">
+  <button className="px-6 py-3 bg-sky-600   hover:bg-sky-700 text-white rounded-full font-bold shadow-lg transition-transform transform hover:scale-105">
+    Confirmar Presença
+  </button>
+</div>
 
-        
-
-        </div>
-
-
-
-
-<button className='
-                rounded-full 
-                absolute 
-                w-[190px] h-[60px] 
-                bg-[#50bcff]
-                text-[#ffffff]
-                cursor-pointer
-                z-10 
-                items-center text-center flex justify-center
-                top-250 left-420
-                 shadow_nim
-                 font-bold 
-                 text-xl
-
-                  hover:scale-105 transition-all duration-300
-                 '
-                  
-                    >Turma:2md</button>
-        </div>
-            </div>
-        </>
-    );
-
+      </div>
+    </div>
+  );
 }
