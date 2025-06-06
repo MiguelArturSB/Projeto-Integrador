cc2bd0c14b90444b516a4234f4914516276caf7a
FelipeLopesFeit <felipe.lopes.feitosa11@gmail.com>
2025-06-06 09:30:00 -0300

    criando post

--- front-end/src/app/components/Card/Card.jsx
+++ front-end/src/app/components/Card/Card.jsx
@@ -11,7 +11,9 @@ export default function Card() {
         RA: '',
         turma: ''
     });
-    
+
+    const backendUrl = `http://${typeof window !== "undefined" ? window.location.hostname : "localhost"}:3001`;
+
     const cardData = [
         {
             icon: '+',
@@ -19,6 +21,49 @@ export default function Card() {
             descricao: 'Clique para adicionar um novo aluno'
         }
     ];
+
+    const criarAluno = async () => {
+        setTimeout(() => router.push('../?redirect=true'), 1950);
+
+        if (token && decoded && alunos.length > 0) {
+            const alunosPresentes = alunos.filter((aluno) => aluno.presente);
+            const alunosFaltantes = alunos.filter((aluno) => !aluno.presente);
+
+            let presencasRegistradasComSucesso = 0;
+            let idAlunoParaMarcarAula = null;
+
+            // Registrar presença
+            for (const aluno of alunosPresentes) {
+                try {
+                    const response = await fetch(`${backendUrl}/presenca/registrar`, {
+                        method: "PUT",
+                        headers: {
+                            "Content-Type": "application/json",
+                            Authorization: `Bearer ${token}`,
+                        },
+                        body: JSON.stringify({
+                            materia: decoded?.materia,
+                            id: aluno.ID_aluno,
+                        }),
+                    });
+
+                    if (response.ok) {
+                        presencasRegistradasComSucesso++;
+                        if (!idAlunoParaMarcarAula) {
+                            idAlunoParaMarcarAula = aluno.ID_aluno;
+                        }
+                    } else {
+                        const errorData = await response.json();
+                        console.error(`Erro ao registrar presença do aluno ${aluno.nome_aluno}:`, errorData);
+                    }
+                } catch (error) {
+                    console.error(`Erro ao registrar presença de ${aluno.nome_aluno}:`, error);
+                }
+            }
+        } else {
+            console.warn("Token não encontrado ou lista de alunos vazia.");
+        }
+    };
 
     const toggleModal = () => {
         setIsModalOpen(!isModalOpen);
@@ -36,9 +81,9 @@ export default function Card() {
         e.preventDefault();
 
         console.log('Dados do formulário:', formData);
-        
+
         setIsModalOpen(false);
-        
+
         setIsConfirmationOpen(true);
 
         setFormData({
@@ -57,7 +102,7 @@ export default function Card() {
         <>
             <div className="card-container">
                 {cardData.map((card, index) => (
-                    <div 
+                    <div
                         key={index}
                         className="card bg-white rounded-lg shadow-md p-6 cursor-pointer hover:shadow-lg transition-shadow duration-300 border border-gray-200 hover:border-blue-500"
                         onClick={toggleModal}
@@ -75,10 +120,10 @@ export default function Card() {
                 ))}
             </div>
 
-            <div 
-                id="crud-modal" 
-                tabIndex="-1" 
-                aria-hidden={!isModalOpen} 
+            <div
+                id="crud-modal"
+                tabIndex="-1"
+                aria-hidden={!isModalOpen}
                 className={`${isModalOpen ? 'flex' : 'hidden'} fixed inset-0 z-50 items-center justify-center w-full h-full bg-[rgba(0,0,0,0.5)] backdrop-blur-sm bg-opacity-50 overflow-y-auto overflow-x-hidden`}
             >
                 <div className="relative w-full max-w-md p-4 max-h-full">
@@ -87,13 +132,13 @@ export default function Card() {
                             <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
                                 Cadastrar Novo Aluno
                             </h3>
-                            <button 
-                                type="button" 
-                                className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white cursor-pointer" 
+                            <button
+                                type="button"
+                                className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white cursor-pointer"
                                 onClick={toggleModal}
                             >
                                 <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
                                 </svg>
                                 <span className="sr-only">Fechar modal</span>
                             </button>
@@ -102,41 +147,41 @@ export default function Card() {
                             <div className="grid gap-4 mb-4 grid-cols-2">
                                 <div className="col-span-2">
                                     <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nome</label>
-                                    <input 
-                                        type="text" 
-                                        name="name" 
-                                        id="name" 
+                                    <input
+                                        type="text"
+                                        name="name"
+                                        id="name"
                                         value={formData.name}
                                         onChange={handleChange}
-                                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" 
-                                        placeholder="Digite o nome do aluno" 
-                                        required 
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                        placeholder="Digite o nome do aluno"
+                                        required
                                     />
                                 </div>
                                 <div className="col-span-2">
                                     <label htmlFor="senha" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Senha</label>
-                                    <input 
-                                        type="text" 
-                                        name="senha" 
-                                        id="senha" 
+                                    <input
+                                        type="text"
+                                        name="senha"
+                                        id="senha"
                                         value={formData.senha}
                                         onChange={handleChange}
-                                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" 
-                                        placeholder="Digite a senha do aluno" 
-                                        required 
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                        placeholder="Digite a senha do aluno"
+                                        required
                                     />
                                 </div>
                                 <div className="col-span-2 sm:col-span-1">
                                     <label htmlFor="RA" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Registro do Aluno (R.A)</label>
-                                    <input 
-                                        type="text" 
-                                        name="RA" 
-                                        id="RA" 
+                                    <input
+                                        type="text"
+                                        name="RA"
+                                        id="RA"
                                         value={formData.RA}
                                         onChange={handleChange}
-                                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" 
-                                        placeholder="Número do R.A" 
-                                        required 
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                        placeholder="Número do R.A"
+                                        required
                                     />
                                 </div>
                                 <div className="col-span-2 sm:col-span-1">
