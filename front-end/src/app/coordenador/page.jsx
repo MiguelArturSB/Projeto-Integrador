d40106660987646769842776db4c4ffeee8d8251
carolinda <anacarollinirossi@gmail.com>
2025-06-06 09:25:40 -0300

    cards

--- front-end/src/app/coordenador/page.jsx
+++ front-end/src/app/coordenador/page.jsx
@@ -228,7 +228,7 @@ export default function Coordenador() {
                 <h1 className="font-semibold">Gerenciar</h1>
                 <button
                     onClick={() => setModalAberto(true)}
-                    className="cursor-pointer font-medium bg-[#1f557b] hover:bg-[#0e3754] transition-all duration-300 border-2 border-solid border-blue-200 mt-4 text-white hover:text-gray-500 text-[12px] text-center p-3 rounded-full h-11"
+                    className="cursor-pointer font-bold bg-[#1f557b] hover:bg-[#0e3754] transition-all duration-300 border-2 border-solid border-blue-200 mt-4 text-white hover:text-gray-500 text-[12px] text-center p-3 rounded-full h-11"
                 >
                     Buscar Aluno...
                 </button>
