0a735890f81d386714c1b2ba112bf2e27d3a152a
MiguelAr <miguelarthurdasilvabarros@gmail.com>
2025-06-08 21:53:41 -0300

    arrumadinha

--- front-end/src/app/page.js
+++ front-end/src/app/page.js
@@ -420,7 +420,7 @@ export default function Portal() {
                   <>
                     <p>Esqueceu sua senha? Redefina através do e-mail institucional.</p>
                     <div className="mt-4 text-center text-sm text-gray-600">
-                      {(activeForm === 'professor' || activeForm === 'coordenacao') && (
+                      {(activeForm === 'professor' || activeForm === 'coordenador') && (
                         <>
                           <hr className="my-4 h-0.5 border-t-0 bg-neutral-100 dark:bg-white/10" />
                           <p
